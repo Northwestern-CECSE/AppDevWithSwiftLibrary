cc7463ef1db2944d4e86aae3170140cb4bbd89db
Kevin McQuown <kevin@thewcl.com>
2021-04-22 14:29:39 -0500

    added image picker

---
+++ Sources/AppDevWithSwiftLibrary/ImagePicker.swift
@@ -0,0 +1,40 @@
+import Foundation
+import SwiftUI
+
+public class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @Binding var isShown: Bool
+    @Binding var image: Image?
+    
+    init(isShown: Binding<Bool>, image: Binding<Image?>) {
+        _isShown = isShown
+        _image = image
+    }
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let uiImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        image = Image(uiImage: uiImage)
+        isShown = false  // close the image picker controller
+    }
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isShown = false
+    }
+}
+public struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var isShown: Bool
+    @Binding var image: Image?
+    
+    public func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+         
+    }
+    
+    public func makeCoordinator() -> ImagePickerCoordinator {
+        return ImagePickerCoordinator(isShown: $isShown, image: $image)
+    }
+    
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+}
