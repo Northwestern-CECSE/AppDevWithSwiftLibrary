f222c13455d95568912b04e183038f2f68d5187b
Kevin McQuown <kevin@thewcl.com>
2021-04-26 07:15:24 -0500

    added webkit support

---
+++ Sources/AppDevWithSwiftLibrary/WebView.swift
@@ -0,0 +1,30 @@
+//
+//  WebView.swift
+//  WebView
+//
+//  Created by Kevin McQuown on 4/26/21.
+//
+
+import SwiftUI
+import WebKit
+
+public struct SwiftUIWebView: UIViewRepresentable {
+    
+    let url: URL?
+    
+    public func makeUIView(context: Context) -> some WKWebView {
+        let prefs = WKWebpagePreferences()
+        prefs.allowsContentJavaScript = true
+        let config  = WKWebViewConfiguration()
+        config.defaultWebpagePreferences = prefs
+        return WKWebView(frame: .zero, configuration: config)
+    }
+    
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        guard let myURL  = url else {
+            return
+        }
+        let request = URLRequest(url: myURL)
+        uiView.load(request)
+    }
+}
