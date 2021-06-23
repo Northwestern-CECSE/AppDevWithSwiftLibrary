e6d44e4efc28407bcff9bba236b5178ef7477817
Kevin McQuown <kevin@thewcl.com>
2021-06-23 14:48:56 -0500

    added ability to send back video url

--- Sources/AppDevWithSwiftLibrary/ImagePicker.swift
+++ Sources/AppDevWithSwiftLibrary/ImagePicker.swift
@@ -6,17 +6,22 @@ public class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, U
     @Binding public var isShown: Bool
     @Binding public var image: Image?
     @Binding public var imagePNG: UIImage?
+    @Binding public var videoURL: URL?
     
-    public init(isShown: Binding<Bool>, image: Binding<Image?>, imagePNG: Binding<UIImage?>) {
+    public init(isShown: Binding<Bool>, image: Binding<Image?>, imagePNG: Binding<UIImage?>, videoURL: Binding<URL?>) {
         _isShown = isShown
         _image = image
         _imagePNG = imagePNG
+        _videoURL = videoURL
     }
     
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let uiImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
-        imagePNG = uiImage
-        image = Image(uiImage: uiImage)
+        if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePNG = uiImage
+            image = Image(uiImage: uiImage)
+        } else {
+            videoURL = info[UIImagePickerController.InfoKey.mediaURL] as? URL
+        }
         isShown = false  // close the image picker controller
     }
     public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -31,12 +36,14 @@ public struct ImagePicker: UIViewControllerRepresentable {
     @Binding public var isShown: Bool
     @Binding public var image: Image?
     @Binding public var imagePNG: UIImage?
+    @Binding public var videoURL: URL?
     public var source: SourceType
     
-    public init(isShown: Binding<Bool>, image: Binding<Image?>, imagePNG: Binding<UIImage?>, source: SourceType) {
+    public init(isShown: Binding<Bool>, image: Binding<Image?>, imagePNG: Binding<UIImage?>, videoURL:Binding<URL?>, source: SourceType) {
         _isShown = isShown
         _image = image
         _imagePNG = imagePNG
+        _videoURL = videoURL
         self.source = source
     }
     
@@ -51,7 +58,7 @@ public struct ImagePicker: UIViewControllerRepresentable {
     }
     
     public func makeCoordinator() -> ImagePickerCoordinator {
-        return ImagePickerCoordinator(isShown: $isShown, image: $image, imagePNG: $imagePNG)
+        return ImagePickerCoordinator(isShown: $isShown, image: $image, imagePNG: $imagePNG, videoURL: $videoURL)
     }
     
     public func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
